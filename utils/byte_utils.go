cecaf827916a2531f7abe06de568374a123848e4
refaeli <refaeliziv@gmail.com>
2024-10-17 15:17:42 +0300

    Add GenerateData byte util

--- utils/byte_utils.go
+++ utils/byte_utils.go
@@ -3,3 +3,13 @@ package utils
 func ConcatBytes(higher, lower byte) uint16 {
 	return (uint16(higher) << 8) | uint16(lower)
 }
+
+func GenerateData(bufferSize uint16) []byte {
+	data := make([]byte, bufferSize)
+	var i uint16 = 0
+
+	for ; i < bufferSize; i++ {
+		data[i] = byte(97 + i%23)
+	}
+	return data
+}
