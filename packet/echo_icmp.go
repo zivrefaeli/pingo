b198ed7a770184206de60ca7d748a10fd5087963
refaeli <refaeliziv@gmail.com>
2024-10-17 15:05:15 +0300

    Implement EchoICMP CalcChecksum with Internet checksum

--- packet/echo_icmp.go
+++ packet/echo_icmp.go
@@ -1,5 +1,10 @@
 package packet
 
+import (
+	"math"
+	"pingo/utils"
+)
+
 type EchoICMP struct {
 	ICMP
 	Identifier uint16
@@ -11,5 +16,26 @@ func (p *EchoICMP) Parse() []byte {
 }
 
 func (p *EchoICMP) CalcChecksum() {
-	p.Checksum = 1
+	var headersSum uint16 = utils.ConcatBytes(p.Type, p.Code) + p.Identifier + p.Sequence
+	var sumCarry uint16
+
+	dataSize := len(p.Data)
+	var dataHigher, dataLower byte
+
+	for i := 0; i < dataSize; i += 2 {
+		dataHigher = p.Data[i]
+		if i+1 < dataSize {
+			dataLower = p.Data[i+1]
+		} else {
+			dataLower = 0
+		}
+		diff := utils.ConcatBytes(dataHigher, dataLower)
+
+		if int(headersSum)+int(diff) > math.MaxUint16 {
+			sumCarry++
+		}
+		headersSum += diff
+	}
+
+	p.Checksum = ^(headersSum + sumCarry)
 }
