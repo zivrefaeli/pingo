4a68580ed2fdaa526b61cb4ad2d40080a743fe02
Ziv Refaeli <76972862+zivrefaeli@users.noreply.github.com>
2024-10-17 23:36:53 +0300

    Feat/icmp packet (#1)
    * Initialize go module
    
    * Create ICMP struct with Packet interface
    
    * Create EchoICMP struct for ICMP Request/Reply variant
    
    * Add ConcatBytes byte utils
    
    * Implement EchoICMP CalcChecksum with Internet checksum
    
    * Add GenerateData byte util
    
    * Create a packet sample
    
    * Implement EchoICMP Parse method
    
    * Display EchoICMP Parse example
    
    * Re-range variables declaration
    
    * Avoid magic numbers

---
+++ packet/echo_icmp.go
@@ -0,0 +1,54 @@
+package packet
+
+import (
+	"math"
+	"pingo/utils"
+)
+
+type EchoICMP struct {
+	ICMP
+	Identifier uint16
+	Sequence   uint16
+}
+
+func (p *EchoICMP) Parse() []byte {
+	p.CalcChecksum()
+
+	icmpHeader := make([]byte, 8)
+	icmpHeader[0] = p.Type
+	icmpHeader[1] = p.Code
+	icmpHeader[2] = byte(p.Checksum >> 8)
+	icmpHeader[3] = byte(p.Checksum)
+	icmpHeader[4] = byte(p.Identifier >> 8)
+	icmpHeader[5] = byte(p.Identifier)
+	icmpHeader[6] = byte(p.Sequence >> 8)
+	icmpHeader[7] = byte(p.Sequence)
+
+	icmpHeader = append(icmpHeader, p.Data...)
+
+	return icmpHeader
+}
+
+func (p *EchoICMP) CalcChecksum() {
+	dataSize := len(p.Data)
+	var dataHigher, dataLower byte
+	var sumCarry uint16
+	headersSum := utils.ConcatBytes(p.Type, p.Code) + p.Identifier + p.Sequence
+
+	for i := 0; i < dataSize; i += 2 {
+		dataHigher = p.Data[i]
+		if i+1 < dataSize {
+			dataLower = p.Data[i+1]
+		} else {
+			dataLower = 0
+		}
+		diff := utils.ConcatBytes(dataHigher, dataLower)
+
+		if int(headersSum)+int(diff) > math.MaxUint16 {
+			sumCarry++
+		}
+		headersSum += diff
+	}
+
+	p.Checksum = ^(headersSum + sumCarry)
+}
