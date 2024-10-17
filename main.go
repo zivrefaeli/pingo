9feb405b8e940af8f2081b78511bc9e742bb83b4
refaeli <refaeliziv@gmail.com>
2024-10-17 15:17:59 +0300

    Create a packet sample

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"pingo/packet"
+	"pingo/utils"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	var size uint16 = 100
+
+	pRequest := packet.EchoICMP{
+		ICMP: packet.ICMP{
+			Type: 8,
+			Code: 0,
+			Data: utils.GenerateData(size),
+		},
+		Identifier: 1,
+		Sequence:   54,
+	}
+
+	pRequest.CalcChecksum()
+
+	fmt.Printf("0x%x\n", pRequest.Checksum)
 }
