db655309769883b3db766239be331684a64e1f20
refaeli <refaeliziv@gmail.com>
2024-10-18 16:22:55 +0300

    Update error handling in SendPacket

--- main.go
+++ main.go
@@ -10,19 +10,17 @@ import (
 
 const IP_HEADER_SIZE = 20
 
-func SendPacket(conn *net.Conn, request *packet.EchoICMP) {
+func SendPacket(conn *net.Conn, request *packet.EchoICMP) (int64, error) {
 	startTime := time.Now()
 	sentBytes, err := (*conn).Write(request.Parse())
 	if err != nil {
-		fmt.Println(err)
-		return
+		return -1, err
 	}
 	buffer := make([]byte, IP_HEADER_SIZE+sentBytes)
 
 	_, err = (*conn).Read(buffer)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return -1, err
 	}
 	timeDiff := time.Since(startTime)
 	ipHeader := buffer[:IP_HEADER_SIZE]
@@ -38,17 +36,24 @@ func SendPacket(conn *net.Conn, request *packet.EchoICMP) {
 		Identifier: utils.ConcatBytes(icmpHeader[4], icmpHeader[5]),
 		Sequence:   utils.ConcatBytes(icmpHeader[6], icmpHeader[7]),
 	}
-	if response.Checksum == response.CalcChecksum() {
-		fmt.Printf("Reply from %s: bytes=%d time=%dms TTL=%d\n", (*conn).RemoteAddr().String(), len(response.Data), timeDiff.Milliseconds(), ipHeader[8])
-	} else {
-		fmt.Printf("Invalid checksum response 0x%x\n", response.Checksum)
+
+	if response.Checksum != response.CalcChecksum() {
+		return -1, fmt.Errorf("invalid checksum response 0x%x", response.Checksum)
 	}
+
+	ms := timeDiff.Milliseconds()
+	if ms == 0 {
+		fmt.Printf("Reply from %s: bytes=%d time<1ms TTL=%d\n", (*conn).RemoteAddr().String(), len(response.Data), ipHeader[8])
+	} else {
+		fmt.Printf("Reply from %s: bytes=%d time=%dms TTL=%d\n", (*conn).RemoteAddr().String(), len(response.Data), ms, ipHeader[8])
+	}
+	return ms, nil
 }
 
 func main() {
 	address := "127.0.0.1"
 	var size uint16 = 32
-	count := 4
+	totalPackets := 4
 
 	request := packet.EchoICMP{
 		ICMP: packet.ICMP{
@@ -68,10 +73,13 @@ func main() {
 	defer conn.Close()
 	fmt.Printf("\nPinging %s with %d bytes of data:\n", address, size)
 
-	for i := 0; i < count; i++ {
-		SendPacket(&conn, &request)
+	for i := 0; i < totalPackets; i++ {
+		_, err := SendPacket(&conn, &request)
+		if err != nil {
+			fmt.Println(err)
+		}
 
-		if i < count-1 {
+		if i < totalPackets-1 {
 			time.Sleep(time.Second)
 			request.Sequence++
 		}
