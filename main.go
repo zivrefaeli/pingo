b7413f12e476703346be1c5bc8b0e204d1873eb2
refaeli <refaeliziv@gmail.com>
2024-10-18 13:45:34 +0300

    Open ip connection to send ICMP packets

--- main.go
+++ main.go
@@ -2,22 +2,29 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"pingo/packet"
 	"pingo/utils"
 )
 
 func main() {
+	address := "127.0.0.1"
+	var size uint16 = 32
+
 	request := packet.EchoICMP{
 		ICMP: packet.ICMP{
 			Type: 8,
 			Code: 0,
-			Data: utils.GeneratePingData(30),
+			Data: utils.GeneratePingData(size),
 		},
 		Identifier: 1,
 		Sequence:   10,
 	}
 
-	fmt.Println(request)
-	fmt.Println(request.Parse())
-	fmt.Printf("Checksum = 0x%x\n", request.Checksum)
+	conn, err := net.Dial("ip:icmp", address)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close()
 }
