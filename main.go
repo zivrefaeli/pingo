2a432d3632902f3be06971ef31a621242e7ec807
Ziv Refaeli <76972862+zivrefaeli@users.noreply.github.com>
2024-10-19 18:40:41 +0300

    Feat/cli (#3)
    * Install cobra CLI framework
    
    * Get targetName from positional arguments
    
    * Add count and size flags for echoRequestsCount and bufferSize
    
    * Make sure only one argument is passed
    
    * Add short command description
    
    * Update README.md

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"os"
 	"pingo/packet"
+
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	packet.StartPinging("google.com", 4, 32)
+	var echoRequestsCount int
+	var bufferSize uint16
+
+	rootCmd := &cobra.Command{
+		Use:   "pingo [TARGET_NAME]",
+		Short: "send ICMP ECHO_REQUEST to network hosts",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			targetName := args[0]
+			return packet.StartPinging(targetName, echoRequestsCount, bufferSize)
+		},
+		SilenceUsage: true,
+	}
+
+	rootCmd.Flags().IntVarP(&echoRequestsCount, "count", "n", 4, "Number of echo requests to send.")
+	rootCmd.Flags().Uint16VarP(&bufferSize, "size", "l", 32, "Send buffer size.")
+
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
 }
