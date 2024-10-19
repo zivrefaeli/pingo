7ebe78501a30da6c8a7328ed7dd8f0e3bf2664f8
refaeli <refaeliziv@gmail.com>
2024-10-19 13:08:17 +0300

    Export SendPacket to functions to use interfaces correctly

--- packet/icmp.go
+++ packet/icmp.go
@@ -1,5 +1,12 @@
 package packet
 
+import (
+	"net"
+	"pingo/utils"
+)
+
+const IP_HEADER_SIZE = 20
+
 type Packet interface {
 	Parse() []byte
 }
@@ -14,4 +21,27 @@ type ICMP struct {
 	Code     byte
 	Checksum uint16
 	Data     []byte
+}
+
+func SendICMPPacket(conn *net.Conn, icmp ICMPPacket) (ICMP, error) {
+	sentBytes, err := (*conn).Write(icmp.Parse())
+	if err != nil {
+		return ICMP{}, err
+	}
+
+	buffer := make([]byte, IP_HEADER_SIZE+sentBytes)
+
+	_, err = (*conn).Read(buffer)
+	if err != nil {
+		return ICMP{}, err
+	}
+
+	icmpHeader := buffer[IP_HEADER_SIZE:]
+
+	return ICMP{
+		Type:     icmpHeader[0],
+		Code:     icmpHeader[1],
+		Checksum: utils.ConcatBytes(icmpHeader[2], icmpHeader[3]),
+		Data:     icmpHeader[4:],
+	}, nil
 }
