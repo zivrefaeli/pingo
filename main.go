b61870ae885534dadf49e30bc618783f4e7ca7ec
refaeli <refaeliziv@gmail.com>
2024-10-19 16:37:38 +0300

    Get targetName from positional arguments

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"os"
 	"pingo/packet"
+
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	packet.StartPinging("google.com", 4, 32)
+	rootCmd := &cobra.Command{
+		Use: "pingo",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			targetName := args[0]
+			return packet.StartPinging(targetName, 4, 32)
+		},
+	}
+
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
 }
