740eeefa2032babd06117c3ec37238579c262e9c
refaeli <refaeliziv@gmail.com>
2024-10-19 16:41:07 +0300

    Add count and size flags for echoRequestsCount and bufferSize

--- main.go
+++ main.go
@@ -8,13 +8,19 @@ import (
 )
 
 func main() {
+	var echoRequestsCount int
+	var bufferSize uint16
+
 	rootCmd := &cobra.Command{
 		Use: "pingo",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			targetName := args[0]
-			return packet.StartPinging(targetName, 4, 32)
+			return packet.StartPinging(targetName, echoRequestsCount, bufferSize)
 		},
 	}
+
+	rootCmd.Flags().IntVarP(&echoRequestsCount, "count", "n", 4, "Number of echo requests to send.")
+	rootCmd.Flags().Uint16VarP(&bufferSize, "size", "l", 32, "Send buffer size.")
 
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
